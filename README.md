130f55861f62b60a915009606e3a33e32cda35fa
Parabeac-Nest <admin@parabeac.com>
2022-11-18 15:55:29 +0000

    Auto-generated commit by Parabeac Cloud

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # wonderapp
 
-A new wonderful Flutter project.
+A new Flutter project.
 
 ## Getting Started
 
---
+++ lib/theme/wonderapp_theme.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:wonderapp/theme/wonderapp_text_styles.g.dart';
+import 'package:wonderapp/theme/wonderapp_colors.g.dart';
+
+class WonderappTheme {
+
+static const TextTheme textTheme = TextTheme(headline1: WonderappTextStyles.headline1,headline2: WonderappTextStyles.headline2,headline3: WonderappTextStyles.headline3,headline4: WonderappTextStyles.headline4,headline5: WonderappTextStyles.headline5,headline6: WonderappTextStyles.headline6,subtitle1: WonderappTextStyles.subtitle1,subtitle2: WonderappTextStyles.subtitle2,bodyText1: WonderappTextStyles.bodyText1,bodyText2: WonderappTextStyles.bodyText2,button: WonderappTextStyles.button,caption: WonderappTextStyles.caption,overline: WonderappTextStyles.overline,);
+
+static const ColorScheme light = ColorScheme.light(onPrimary: WonderappColors.lightOnPrimary,primary: WonderappColors.lightPrimary,primaryContainer: WonderappColors.lightPrimaryContainer,secondary: WonderappColors.lightSecondary,onSecondary: WonderappColors.lightOnSecondary,onPrimaryContainer: WonderappColors.lightOnPrimaryContainer,secondaryContainer: WonderappColors.lightSecondaryContainer,onSecondaryContainer: WonderappColors.lightOnSecondaryContainer,onTertiary: WonderappColors.lightOnTertiary,tertiary: WonderappColors.lightTertiary,tertiaryContainer: WonderappColors.lightTertiaryContainer,onTertiaryContainer: WonderappColors.lightOnTertiaryContainer,error: WonderappColors.lightError,onError: WonderappColors.lightOnError,errorContainer: WonderappColors.lightErrorContainer,onErrorContainer: WonderappColors.lightOnErrorContainer,background: WonderappColors.lightBackground,onBackground: WonderappColors.lightOnBackground,surface: WonderappColors.lightSurface,onSurface: WonderappColors.lightOnSurface,surfaceVariant: WonderappColors.lightSurfaceVariant,onSurfaceVariant: WonderappColors.lightOnSurfaceVariant,outline: WonderappColors.lightOutline,inverseSurface: WonderappColors.lightInverseSurface,onInverseSurface: WonderappColors.lightOnInverseSurface,inversePrimary: WonderappColors.lightInversePrimary,surfaceTint: WonderappColors.lightSurfaceTint);
+
+static  ThemeData themeDataLight = ThemeData(textTheme: textTheme, colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: WonderappColors.darkPrimary,onPrimary: WonderappColors.darkOnPrimary,primaryContainer: WonderappColors.darkPrimaryContainer,onPrimaryContainer: WonderappColors.darkOnPrimaryContainer,secondary: WonderappColors.darkSecondary,onSecondary: WonderappColors.darkOnSecondary,secondaryContainer: WonderappColors.darkSecondaryContainer,onSecondaryContainer: WonderappColors.darkOnSecondaryContainer,tertiary: WonderappColors.darkTertiary,onTertiary: WonderappColors.darkOnTertiary,tertiaryContainer: WonderappColors.darkTertiaryContainer,onTertiaryContainer: WonderappColors.darkOnTertiaryContainer,error: WonderappColors.darkError,onError: WonderappColors.darkOnError,errorContainer: WonderappColors.darkErrorContainer,onErrorContainer: WonderappColors.darkOnErrorContainer,background: WonderappColors.darkBackground,onBackground: WonderappColors.darkOnBackground,surface: WonderappColors.darkSurface,onSurface: WonderappColors.darkOnSurface,surfaceVariant: WonderappColors.darkSurfaceVariant,outline: WonderappColors.darkOutline,inverseSurface: WonderappColors.darkInverseSurface,inversePrimary: WonderappColors.darkInversePrimary,surfaceTint: WonderappColors.darkSurfaceTint,onSurfaceVariant: WonderappColors.darkOnSurfaceVariant,onInverseSurface: WonderappColors.darkOnInverseSurface);
+
+static  ThemeData themeDataDark = ThemeData(textTheme: textTheme, colorScheme: dark,);
+}
